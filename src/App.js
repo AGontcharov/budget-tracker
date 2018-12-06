2befb7aa0847d9e01d8a6f9516cfdcff127b9c3c
Alexander Gontcharov <alexander.goncharov@gmail.com>
2018-12-05 23:28:56 -0500

    Add React Strict Mode

--- src/App.js
+++ src/App.js
@@ -46,25 +46,27 @@ class App extends React.Component<Props> {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <Layout>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton color="inherit" aria-label="Menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" color="inherit">
-                Budget Tracker
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <div style={styles}>
-            <DropFile />
-            <BudgetTable />
-            <Dashboard />
-          </div>
+        <React.StrictMode>
+          <Layout>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton color="inherit" aria-label="Menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit">
+                  Budget Tracker
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <div style={styles}>
+              <DropFile />
+              <BudgetTable />
+              <Dashboard />
+            </div>
 
-          <Footer />
-        </Layout>
+            <Footer />
+          </Layout>
+        </React.StrictMode>
       </MuiThemeProvider>
     );
   }
