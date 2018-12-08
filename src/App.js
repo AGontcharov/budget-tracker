f82adfeebd4a0f27a008023ae872380b5b8550a1
Alexander Gontcharov <alexander.goncharov@gmail.com>
2018-12-07 23:56:28 -0500

    Add DrawerMenu Component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import * as React from 'react';
 // Custom Component
 import BudgetTable from 'budgetTable';
 import Dashboard from 'dashboard';
+import DrawerMenu from 'layout/DrawerMenu';
 import Layout from 'layout/Layout';
 import Footer from 'layout/Footer';
 import DropFile from 'components/DropFile';
 
 // Material UI
-import AppBar from '@material-ui/core/AppBar';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+// Icons
+import MenuIcon from '@material-ui/icons/Menu';
+
 type Props = {};
+
+type State = {
+  open: boolean
+};
 
 const theme = createMuiTheme({
   palette: {
@@ -42,15 +49,29 @@ const styles = {
   justifyContent: 'center'
 };
 
-class App extends React.Component<Props> {
+class App extends React.Component<Props, State> {
+  state = {
+    open: false
+  };
+
+  onClick = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  onClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { open } = this.state;
+
     return (
       <MuiThemeProvider theme={theme}>
         {/* <React.StrictMode> */}
         <Layout>
           <AppBar position="static">
             <Toolbar>
-              <IconButton color="inherit" aria-label="Menu">
+              <IconButton color="inherit" aria-label="Menu" onClick={this.onClick}>
                 <MenuIcon />
               </IconButton>
               <Typography variant="h6" color="inherit">
@@ -58,6 +79,7 @@ class App extends React.Component<Props> {
               </Typography>
             </Toolbar>
           </AppBar>
+          <DrawerMenu open={open} onClose={this.onClose} />
           <div style={styles}>
             <DropFile />
             <BudgetTable />
