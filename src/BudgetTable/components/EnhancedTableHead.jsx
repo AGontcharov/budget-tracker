22012d68e06f12ae158f2be53e57066c3eb84a26
Alexander Gontcharov <alexander.goncharov@gmail.com>
2018-12-26 18:21:31 -0500

    Add description icon to column header

--- src/BudgetTable/components/EnhancedTableHead.jsx
+++ src/BudgetTable/components/EnhancedTableHead.jsx
@@ -2,27 +2,39 @@
 import * as React from 'react';
 
 // Material UI
+import { withStyles } from '@material-ui/core/styles';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Tooltip from '@material-ui/core/Tooltip';
 
+import CommentIcon from '@material-ui/icons/Comment';
+
 // Helper Functions
 import { headers } from 'lib/Utils';
 
 type Props = {
+  classes: {
+    icon: string
+  },
   onRequestSort: string => void,
   order: string,
   orderBy: string
 };
+
+const styles = theme => ({
+  icon: {
+    marginRight: theme.spacing.unit
+  }
+});
 
 const EnhancedTableHead = (props: Props) => {
   const createSortHandler = (property: string) => (event: SyntheticEvent<>) => {
     props.onRequestSort(property);
   };
 
-  const { order, orderBy } = props;
+  const { classes, order, orderBy } = props;
 
   return (
     <TableHead>
@@ -45,6 +57,9 @@ const EnhancedTableHead = (props: Props) => {
                   direction={order}
                   onClick={createSortHandler(row.id)}
                 >
+                  {row.label === 'Description' && (
+                    <CommentIcon className={classes.icon} fontSize="small" />
+                  )}
                   {row.label}
                 </TableSortLabel>
               </Tooltip>
@@ -56,4 +71,4 @@ const EnhancedTableHead = (props: Props) => {
   );
 };
 
-export default EnhancedTableHead;
+export default withStyles(styles, { withTheme: true })(EnhancedTableHead);
